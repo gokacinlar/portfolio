40f67f3ff44de2565014ff7c64accdd9cdfa5bc8
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-06-11 11:34:52 +0300

    add: day-night btn change on click (not functioning atm)

--- src/helper.ts
+++ src/helper.ts
@@ -8,3 +8,35 @@ export class Template {
         return template;
     }
 }
+
+// For DOM Manipulation
+
+export class DomEvents {
+    public headerRightActions(elem: HTMLButtonElement) {
+        const textSpan = elem.querySelector(".hr-btn-text") as HTMLElement;
+        elem.addEventListener("mouseover", () => {
+            textSpan.textContent = "For your web demands...";
+        });
+
+        elem.addEventListener("mouseleave", () => {
+            textSpan.textContent = "Hire me!";
+        });
+    }
+
+    public dayNightModeSwitching(elem: HTMLButtonElement, elemToBeManip: string) {
+        // Flag for reversing roles
+        let reset = true;
+        elem.addEventListener("click", () => {
+            const dayNightModeSwitchingBtn = document.querySelector(`${elemToBeManip}`) as HTMLElement;
+            if (reset) {
+                dayNightModeSwitchingBtn.classList.remove("bi-sun");
+                dayNightModeSwitchingBtn.classList.add("bi-moon-stars");
+            } else {
+                dayNightModeSwitchingBtn.classList.remove("bi-moon-stars");
+                dayNightModeSwitchingBtn.classList.add("bi-sun");
+            }
+            // Reset the flag
+            reset = !reset;
+        });
+    }
+}
