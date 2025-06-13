b082212cdaafd2021c11b256a3bec1a0604731f7
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-06-13 21:29:53 +0300

    add: type writer and marquee effects

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,5 @@
+import { HeroParts, PromoParts } from "./static";
+
 // Create a template content to be appended to every Light DOM
 export class Template {
     public createTemplate(content: any): HTMLTemplateElement {
@@ -10,12 +12,11 @@ export class Template {
 }
 
 // For DOM Manipulation
-
 export class DomEvents {
     public headerRightActions(elem: HTMLButtonElement) {
         const textSpan = elem.querySelector(".hr-btn-text") as HTMLElement;
         elem.addEventListener("mouseover", () => {
-            textSpan.textContent = "For your web demands...";
+            textSpan.textContent = "For your education & web demands...";
         });
 
         elem.addEventListener("mouseleave", () => {
@@ -38,5 +39,96 @@ export class DomEvents {
             // Reset the flag
             reset = !reset;
         });
+    }
+}
+
+// Typewriter effect on Hero section
+export class TypeWriterDisplay {
+    private targetElement: HTMLElement
+    private heroParts: HeroParts;
+    private textIndex: number = 0;
+    private charIndex: number = 0;
+    private isDeleting: boolean = false;
+
+    constructor(heroParts: HeroParts, elementId: string) {
+        this.heroParts = heroParts;
+        const targetElement = document.getElementById(elementId);
+
+        if (targetElement) {
+            this.targetElement = targetElement;
+        } else {
+            throw new Error(`Target element not found`);
+        }
+
+        this.targetElement = targetElement;
+        this.type();
+    }
+
+    private type(): void {
+        const currentText = this.heroParts.occupationsData[this.textIndex];
+        let displayedText = currentText.substring(0, this.charIndex);
+
+        const textSpan = this.targetElement.querySelector(".ocps-written-text");
+        if (textSpan) {
+            textSpan.textContent = displayedText;
+        }
+
+        let delay = 100;
+
+        if (!this.isDeleting) {
+            if (this.charIndex < currentText.length) {
+                this.charIndex++;
+            } else {
+                // When full word is written, pause before deleting
+                delay = 2000;
+                this.isDeleting = true;
+            }
+        } else {
+            if (this.charIndex > 0) {
+                this.charIndex--;
+                delay = 50;
+            } else {
+                // Move to next word
+                this.isDeleting = false;
+                this.textIndex = (this.textIndex + 1) % this.heroParts.occupationsData.length;
+                delay = 500;
+            }
+        }
+
+        setTimeout(() => {
+            this.type();
+        }, delay);
+    }
+}
+
+export class PromoFunctions {
+    private promoParts: PromoParts;
+    constructor() {
+        this.promoParts = new PromoParts();
+    }
+
+    public createMarqueeStack(targetSelector: string) {
+        const container = document.querySelector(`${targetSelector}`) as HTMLDivElement;
+        if (!container) {
+            return;
+        }
+
+        for (const item of this.promoParts.marqueeElements) {
+            const anchor = document.createElement("a") as HTMLAnchorElement;
+            anchor.className = "marquee-item";
+            anchor.href = item.href;
+            anchor.setAttribute("target", "_blank");
+
+            const img = document.createElement("img") as HTMLImageElement;
+            img.className = "marquee-item-img img-fluid img-responsive rounded-4";
+            img.title = item.title;
+            img.src = item.imgSrc;
+            img.setAttribute("loading", "lazy");
+            img.setAttribute("decoding", "async");
+
+            anchor.appendChild(img);
+            container.appendChild(anchor);
+        }
+
     }
 }
