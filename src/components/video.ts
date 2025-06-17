b0d2ca048074d00ad44cd59ba04d173f15ccd0bc
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-06-17 22:19:15 +0300

    new: video card component

---
+++ src/components/video.ts
@@ -0,0 +1,47 @@
+interface VideoElementOptions {
+    title: string;
+    webmSrc: string;
+    mp4FallbackSrc: string;
+    posterSrc: string;
+}
+
+export class VideoElement {
+    private title: string;
+    private webmSrc: string;
+    private mp4FallbackSrc: string;
+    private posterSrc: string;
+
+    constructor({ title, webmSrc, mp4FallbackSrc, posterSrc }: VideoElementOptions) {
+        this.title = title;
+        this.webmSrc = webmSrc;
+        this.mp4FallbackSrc = mp4FallbackSrc;
+        this.posterSrc = posterSrc;
+    }
+
+    public render(header: string): string {
+        return `
+            <div id="component_Video" class="position-relative shadow">
+                <video
+                    id="promoVideo"
+                    class="promo-video rounded-5 w-100"
+                    preload="metadata"
+                    poster="${this.posterSrc}"
+                    loading="lazy"
+                    playsinline
+                    disablepictureinpicture
+                    loop
+                    muted
+                    autoplay
+                    aria-labelledby="${this.title}"
+                    aria-describedby="${this.title}"
+                >
+                    <source src="${this.webmSrc}" type="video/webm">
+                    <source src="${this.mp4FallbackSrc}" type="video/mp4">
+                </video>
+                <h4 class="position-absolute top-50 start-50 py-2 translate-middle text-center display-4 fw-bold rounded-4 text-white pe-none">
+                    ${header}
+                </h4>
+            </div>
+        `;
+    }
+}
