933c68412a01d9f3807cb4fa61078daf810576c1
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-07-21 00:35:51 +0300

    add: links for about social links

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -27,6 +27,7 @@ interface ShuffleElement {
 interface SocialsElement {
     name: string;
     icon: string;
+    href: URL;
 }
 
 class About extends HTMLElement {
@@ -65,10 +66,10 @@ class About extends HTMLElement {
     ];
 
     private readonly SocialsElements: SocialsElement[] = [
-        { name: "GitHub", icon: "bi bi-github" },
-        { name: "X/Twitter", icon: "bi bi-twitter-x" },
-        { name: "Hashnode", icon: "bi bi-book" },
-        { name: "E-mail", icon: "bi bi-mailbox" }
+        { name: "GitHub", icon: "bi bi-github", href: new URL("https://github.com/gokacinlar") },
+        { name: "X/Twitter", icon: "bi bi-twitter-x", href: new URL("https://x.com/devDissentNT") },
+        { name: "Hashnode", icon: "bi bi-book", href: new URL("https://gokacinlar.hashnode.dev/") },
+        { name: "E-mail", icon: "bi bi-mailbox", href: new URL("mailto:gokacinlar@yaani.com?subject=To%20Dervi%C5%9F ") }
     ]
 
     private render(): void {
@@ -161,9 +162,9 @@ class About extends HTMLElement {
     private renderSocials(): string {
         return `
             <div class="d-flex flex-row flex-wrap align-items-start justify-content-evenly gap-1">
-                ${this.SocialsElements.map(({ name, icon }) => `
+                ${this.SocialsElements.map(({ name, icon, href }) => `
                     <div class="aside-socials mb-2 rounded-pill shadow-sm flex-grow-1">
-                        <a href="" class="d-flex flex-row align-items-center gap-2 px-2 py-2 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
+                        <a href="${href}" class="d-flex flex-row align-items-center gap-2 px-2 py-2 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" target="_blank" title="${name}">
                             <i class="${icon} fs-3 px-1 py-1"></i>
                             <p class="aside-social-text h6 fw-medium mb-0">${name}</p>
                             <i class="bi bi-arrow-right fs-4"></i>
