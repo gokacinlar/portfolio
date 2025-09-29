6d1f35bbcb8787e835b94b7e46bba5d45e8f3176
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-09-29 22:05:57 +0300

    improve work page

--- src/pages/work.ts
+++ src/pages/work.ts
@@ -16,14 +16,44 @@ class WorkPage extends HTMLElement {
                 <div class="container text-center h-100">
                     <div class="row gy-2 gx-2 align-items-center h-100">
                         <div class="col-xs-12 col-sm-12 col-md-6">
-                            <h1 class="display-1 text-start fw-medium pe-none"><mark class="px-4 py-1 rounded-end-5">Hire</mark> me or see my plans <span class="wavy-text">below.</span></h1>
+                            <h1 class="display-1 text-start fw-medium pe-none"><mark class="px-4 py-1 rounded-end-5">Hire me</mark> or see my plans <span class="wavy-text">below.</span></h1>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-6">
-                            Column
+                            <button id="workScrollDownBtn" class="btn btn-lg rounded-5 shadow-sm fw-medium fs-2 mx-auto d-flex flex-row align-items-center justify-content-center gap-2"
+                            title="Scroll down" type="button">
+                                <span class="work-scroll-down-indicator">See plans</span>
+                                <i class="work-scroll-down-icon bi bi-arrow-down-circle-fill fs-1"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
             </section>
+            <article id="buyOptions" class="px-2 py-2 mx-2 my-2 rounded-5 h-100">
+                ${this.optionsContainer()}
+            </article>
+        `;
+    }
+
+    private optionsContainer(): string {
+        return `
+            <section id="buyOptions">
+                <div>
+                    ${this.navigate()}
+                </div>
+            </section>
+        `;
+    }
+
+    private navigate(): string {
+        return `
+            <ul class="nav nav-pills nav-fill gap-3">
+                <li class="nav-item">
+                    <a class="nav-link active rounded-5 fs-2 fw-medium shadow-sm" aria-current="page" href="#">For Learning English</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link rounded-5 fs-2 fw-medium shadow-sm" href="#">For Web Development</a>
+                </li>
+            </ul>
         `;
     }
 
