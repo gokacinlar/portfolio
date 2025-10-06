ca790e14e16dd7649207f8ff2c15737d979f9c9e
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2025-10-06 23:15:07 +0300

    improve work section

--- src/pages/work.ts
+++ src/pages/work.ts
@@ -19,16 +19,16 @@ class WorkPage extends HTMLElement {
                             <h1 class="display-1 text-start fw-medium pe-none"><mark class="px-4 py-1 rounded-end-5">Hire me</mark> or see my plans <span class="wavy-text">below.</span></h1>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-6">
-                            <button id="workScrollDownBtn" class="btn btn-lg rounded-5 shadow-sm fw-medium fs-2 mx-auto d-flex flex-row align-items-center justify-content-center gap-2"
+                            <a type="button" href="#buyOptions" id="workScrollDownBtn" class="btn btn-lg rounded-5 shadow-sm fw-medium fs-2 mx-auto d-flex flex-row align-items-center justify-content-center gap-2"
                             title="Scroll down" type="button">
                                 <span class="work-scroll-down-indicator">See plans</span>
                                 <i class="work-scroll-down-icon bi bi-arrow-down-circle-fill fs-1"></i>
-                            </button>
+                            </a>
                         </div>
                     </div>
                 </div>
             </section>
-            <article id="buyOptions" class="px-2 py-2 mx-2 my-2 rounded-5 h-100">
+            <article id="buyOptions" class="px-2 py-2 mx-2 my-2 rounded-5 h-100 shadow-sm">
                 ${this.optionsContainer()}
             </article>
         `;
@@ -36,9 +36,14 @@ class WorkPage extends HTMLElement {
 
     private optionsContainer(): string {
         return `
-            <section id="buyOptions">
-                <div>
-                    ${this.navigate()}
+            <section class="py-2 px-2">
+                <div class="container-fluid gy-2">
+                    <div class="row">
+                        ${this.navigate()}
+                    </div>
+                    <div class="row mt-3 h-50">
+                        ${this.content()}
+                    </div>
                 </div>
             </section>
         `;
@@ -46,14 +51,25 @@ class WorkPage extends HTMLElement {
 
     private navigate(): string {
         return `
-            <ul class="nav nav-pills nav-fill gap-3">
-                <li class="nav-item">
-                    <a class="nav-link active rounded-5 fs-2 fw-medium shadow-sm" aria-current="page" href="#">For Learning English</a>
+            <ul class="nav nav-pills nav-fill px-0 gap-3" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active rounded-5 fs-2 fw-medium shadow-sm" aria-current="page" data-bs-toggle="tab" data-bs-target="#englishOptions" type="button" role="tab"
+                    aria-controls="englishOptions" aria-selected="true" id="forEnglish">For Learning English</button>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link rounded-5 fs-2 fw-medium shadow-sm" href="#">For Web Development</a>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link rounded-5 fs-2 fw-medium shadow-sm" data-bs-toggle="tab" data-bs-target="#programmingOptions" type="button" role="tab"
+                    aria-controls="programmingOptions" aria-selected="false" id="forProgramming">For Web Development</button>
                 </li>
             </ul>
+        `;
+    }
+
+    private content(): string {
+        return `
+            <div id="optionsContent" class="bg-secondary-subtle px-3 py-3 rounded-5 shadow-sm">
+                <div class="tab-pane fade show active" id="englishOptions" role="tabpanel" aria-labelledby="forEnglish">...dfgdfg</div>
+                <div class="tab-pane fade" id="programmingOptions" role="tabpanel" aria-labelledby="forProgramming">...qweqwew</div>
+            </div>
         `;
     }
 
